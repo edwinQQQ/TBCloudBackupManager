fffdd73f3253fd563d9ad78739cf1b9b1a7d7755
edwinQQQ <eqi@theknotww.com>
2023-06-20 16:06:46 +0800

    Initial Commit

---
+++ TBCloudBackupManager/TBCloudBackupManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TBCloudBackupManagerApp.swift
+//  TBCloudBackupManager
+//
+//  Created by eqi on 2023/6/20.
+//
+
+import SwiftUI
+
+@main
+struct TBCloudBackupManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
